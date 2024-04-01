6ebab4f2e0daca4491e825a12c481d77ff5ef8a3
gitmah01720 <maheshpandeyindia@gmail.com>
2024-04-01 22:14:40 +0530

    importing libraries for frontend

--- context/Voter.js
+++ context/Voter.js
@@ -1,4 +1,18 @@
 import React from 'react'
+import Web3Modal from 'web3modal'
+import {ethers} from 'ethers'
+import {create as ipfsHttpClient} from 'ipfs-http-client'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+// intarnal imports:
+import { VotingAddrss, VotingAddrssAddressABI } from './constants';
+
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0")
+
+
+
+
 
 const Voter = () => {
   return (
