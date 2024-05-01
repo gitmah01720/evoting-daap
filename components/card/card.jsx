a9cf37679e35316bdd5be685b557b22a6c13d694
gitmah01720 <maheshpandeyindia@gmail.com>
2024-05-02 00:30:40 +0530

    rendering of index page code and css #3:45

--- components/card/card.jsx
+++ components/card/card.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import Image from "next/image";
 
-const card = () => {
-  return <div>card</div>;
+// internal imports:
+import images from "../../assets";
+// import Style from "../card/card"
+import Style from "../card/card.module.css";
+import Button from "../Button/Button";
+
+const card = ({ candidateArray, giveVote }) => {
+  console.log("cards");
+  console.log(candidateArray);
+  return (
+    <div className={Style.card}>
+      {candidateArray.map((el, i) => (
+        <div className={Style.card_box}>
+          <div className={Style.image}>
+            <img src={el[1]} alt="Profile Image" />
+          </div>
+
+          <div className={Style.card_info}>
+            <h2>
+              <p>
+                {el[0]} #CID-{el[4].toNumber()}
+              </p>
+            </h2>
+            <p>
+              Address: {el[6].slice(0, 5)}...{el[6].slice(-3)}
+            </p>
+
+            <p className={Style.total}>Total Votes</p>
+          </div>
+
+          <div className={Style.card_vote}>
+            <p>{el[2].toNumber()}</p>
+          </div>
+
+          <div className={Style.card_button}>
+            <button
+              onClick={() => giveVote({ id: el[4].toNumber(), address: el[6] })}
+            >
+              Give Vote
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default card;
--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,55 @@ import Card from "../components/card/card"
 import image from "../assets/candidate-1.jpg"
 
 
+// connectWallet,
+// voterLength,
+// voterArray,
+// voterAddress,
+// getAllVoterdata,
+// candidateAddress,
+
 const index = () => {
-  const {votingTitle} = useContext(VotingContext)
-  return <div>{votingTitle}</div>;
+  const {
+    checkIfWalletConnected,
+    giveVote,
+    candidateLength,
+    getAllCandidatedata,
+    currentAccount,
+    candidateArray,
+    voterLength
+
+  } = useContext(VotingContext)
+
+  useEffect(() => {
+    checkIfWalletConnected();
+  })
+
+    // countdown to vote ending.
+  return <div className={Style.home}>{currentAccount && (
+    <div className={Style.winner}>
+      <div className = {Style.winner_info}>
+        <div className = {Style.candidate_list}>
+          <p>
+            Candidates: <span>{candidateLength}</span>
+          </p> 
+        </div>
+        <div className={Style.candidate_list}>
+          <p>
+            Voters: <span>{voterLength}</span>
+          </p>
+        </div>
+      </div>
+
+      <div  className = {Style.winner_message}>
+        <small>
+          <Countdown date = {Date.now() + 100000}/>
+        </small>
+      </div>
+    </div>
+  )}
+
+    <Card candidateArray = {candidateArray} giveVote = {giveVote}/>
+  </div>;
 };
 
 export default index;
