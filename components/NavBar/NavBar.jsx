51d01958f9f2391c6788bbc5e6b3a9c56d2791ca
gitmah01720 <maheshpandeyindia@gmail.com>
2024-05-02 18:28:29 +0530

    index page and navigation working, dynamic metamask logging done #4:41

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, {useState,useContext} from "react";
+import Image from "next/image";
+import Link from "next/link";
+import {AiFillUnlock, AiFillLock} from "react-icons/ai"
+
+// internal imports:
+import {VotingContext} from "../../context/Voter";
+import Style from "./NavBar.module.css";
+// import loadConfig from "next/dist/server/config";
+import loading from "../../assets/loading-2.gif"
+import Button from "../Button/Button";
+
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const {connectWallet,error,currentAccount} = useContext(VotingContext);
+
+  const [openNav, setOpenNav] = useState(true);
+
+  const openNavigation = ()=>{
+    if(openNav){
+      setOpenNav(false)
+    }else if(!openNav){
+      setOpenNav(true)
+    }
+  }
+
+
+  return (<div className = {Style.navbar}>
+  {error === "" ? (
+      ""
+    ): (
+      <div className = {Style.message__box}>
+        <div className={Style.message}>
+          <p> {error} </p>
+        </div>
+      </div>
+    )}
+
+    <div className = {Style.navbar_box}>
+      <div className = {Style.title}>
+        <Link href = {{pathname: '/'}}>
+          <Image src = {loading} alt="logo" width={80} height={80} />
+        </Link>
+      </div>
+
+      <div className={Style.connect}>
+        {currentAccount ? (
+          <div>
+          <div className = {Style.connect_flex}>
+            <button onClick={()=> openNavigation()}>
+              `{currentAccount.slice(0,6)} ... {currentAccount.slice(-5)}`
+            </button>
+
+            {currentAccount && (
+              <span>{openNav ? (
+                <AiFillUnlock onClick = {()=> openNavigation()}/>
+              ):
+                <AiFillLock onClick = {()=> openNavigation()}/>
+              }</span>
+            )}
+          </div>
+            {
+              openNav && (
+                <div className = {Style.navigation}>
+                  <p>
+                    <Link href={{pathname:"/"}}> HOME </Link>
+                  </p>
+                  <p>
+                    <Link href={{pathname:"/candidate-regisration"}}> Candiate Registration </Link>
+                  </p>
+                  <p>
+                    <Link href={{pathname:"/allowed-voters"}}> Voter List</Link>
+                  </p>
+                </div>
+              )
+            }
+
+
+          </div>
+        ) :(
+          <button onClick = {()=>connectWallet()}>Connect Wallet</button>
+        )}
+
+      </div>
+    </div>
+  </div>
+  );
+  
 };
 
 export default NavBar;
--- context/constants.js
+++ context/constants.js
@@ -2,5 +2,5 @@
 // reading build json data from create.json file.
 import voting from "./Election.json";
 
-export const VotingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const VotingAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 export const VotingAddressABI = voting.abi;
